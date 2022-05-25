f17e86366542119630b79cfa440b7721aff5583f
Chris Young <ctopheryoung@gmail.com>
2022-05-25 14:50:39 -0700

    Setup Vuex store

---
+++ src/components/ResultsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <p>Count: {{ count }}</p>
+    <p>Local count: {{ localCount }}</p>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'ResultsList',
+  computed: {
+    ...mapState(['count']),
+    localCount() {
+      return this.count + 2
+    }
+  }
+}
+</script> 
