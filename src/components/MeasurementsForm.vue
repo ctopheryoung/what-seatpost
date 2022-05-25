12e34f9d16500bbe70fd19a0437fdfa9eceae654
Chris Young <ctopheryoung@gmail.com>
2022-05-25 15:36:23 -0700

    Connect measurements form to Vuex

--- src/components/MeasurementsForm.vue
+++ src/components/MeasurementsForm.vue
@@ -3,31 +3,80 @@
     <h2>Enter Dropperpost Measurements</h2>
     <section>
       <h3>Routing Style</h3>
-      <button>Internal</button>
-      <button>External</button>
+      <select name="routing-style" v-model="routingStyle">
+        <option value="internal">Internal</option>
+        <option value="external">External</option>
+      </select>
     </section>
   
     <section>
       <h3>Seatpost Diameter</h3>
-      <button>27.2</button>
-      <button>30.9</button>
-      <button>31.6</button>
-      <button>34.9</button>
+      <select name="seatpost-diameter" v-model.number="seatpostDiameter">
+        <option value="27.2">27.2</option>
+        <option value="30.9">30.9</option>
+        <option value="31.6">31.6</option>
+        <option value="34.9">34.9</option>
+      </select>
     </section>
   
     <section>
       <h3>Max Extension</h3>
-      <p>With you current saddle fully extended (aka seated and pedaling up a hill), measure the distance from the top of your seat collar to the center of the saddle rails. Be careful! If you choose a length that is too long, you won't be able to set the extended height low enough.</p>
-      <input type="text" name="maxExtension"> mm
+      <p>In millimeters, measure the distance from the top of your seat collar to the center of the saddle rails. Be sure to take this measurement when your saddle is fully extended, aka the height you have it set at when seated and pedaling up a hill.</p>
+      <input type="number" name="maxExtension" v-model="maxExtension"> mm
     </section>
   
     <section>
       <h3>Max Insertion</h3>
-      <p>Measure the distance from top of you seat collar down to any obstruction that would limit how far into the frame you could insert the post.</p>
-      <input type="text" name="maxInsertion"> mm
+      <p>In millimeters, measure the distance from top of you seat collar down to any obstruction that would limit how far into the frame you could insert the post.</p>
+      <input type="number" name="maxInsertion" v-model="maxInsertion"> mm
     </section>
   </main>
 </template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+
+  export default {
+    computed: {
+      ...mapState(['routingStyle', 'seatpostDiameter', 'maxExtension', 'maxInsertion']),
+      routingStyle: {
+        get () {
+          return this.routingStyle
+        },
+        set (value) {
+          this.setRoutingStyle(value)
+        }
+      },
+      seatpostDiameter: {
+        get () {
+          return this.seatpostDiameter
+        },
+        set (value) {
+          this.setSeatpostDiameter(value)
+        }
+      },
+      maxExtension: {
+        get () {
+          return this.maxExtension
+        },
+        set (value) {
+          this.setMaxExtension(value)
+        }
+      },
+      maxInsertion: {
+        get () {
+          return this.maxInsertion
+        },
+        set (value) {
+          this.setMaxInsertion(value)
+        }
+      }
+    },
+    methods: {
+      ...mapMutations(['setRoutingStyle', 'setSeatpostDiameter', 'setMaxExtension', 'setMaxInsertion'])
+    }
+  }
+</script>
 
 <style scoped>
   main {
